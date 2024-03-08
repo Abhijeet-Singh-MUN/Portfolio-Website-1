e3a5942765f08a1cb0378c53f22714ed2b76530a
Your Name <you@example.com>
2024-03-08 17:17:14 -0330

    first commit

---
+++ src/models/test.jsx
@@ -0,0 +1,12 @@
+import React, { useRef } from 'react'
+import birdScene from '../assets/3d/bird.glb';
+import { useAnimations, useGLTF} from '@react-three/drei';
+import { useFrame , useEffect} from '@react-three/fiber';
+
+const test = () => {
+  return (
+    <mesh eifshiwefwiufujnsgi ref = {useRef}>test</mesh>
+  )
+}
+
+export default test
